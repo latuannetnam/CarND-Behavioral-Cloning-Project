703cba906ab1f96a0ad98e609b494e0221dba777
Tuan Le Anh <latuannetnam@gmail.com>
2017-09-28 10:58:29 +0700

    remove model file

--- drive.py
+++ drive.py
@@ -19,13 +19,14 @@ from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_a
 
 # Fix error with Keras and TensorFlow
 import tensorflow as tf
-tf.python.control_flow_ops = tf
+# tf.python.control_flow_ops = tf
 
 
 sio = socketio.Server()
 app = Flask(__name__)
 model = None
 prev_image_array = None
+
 
 def process_image(image):
     # Normalize the  image pixels to  range -1 to 1
@@ -35,6 +36,7 @@ def process_image(image):
     # Slice bottom 30 pixels to remove hood
     image = image[40:130, :]
     return image
+
 
 @sio.on('telemetry')
 def telemetry(sid, data):
@@ -51,9 +53,10 @@ def telemetry(sid, data):
     image_array = process_image(image_array)
     transformed_image_array = image_array[None, :, :, :]
     # Adjust steering angle to allow for tighter turns.
-    steering_angle = 1.2 * float(model.predict(transformed_image_array, batch_size=1))
-    # Adjust throttle based on speed. 
-    throttle = max(0., 1.0 - float(speed)/30.)
+    steering_angle = 1.2 * \
+        float(model.predict(transformed_image_array, batch_size=1))
+    # Adjust throttle based on speed.
+    throttle = max(0., 1.0 - float(speed) / 30.)
     print(steering_angle, throttle)
     send_control(steering_angle, throttle)
 
@@ -66,15 +69,15 @@ def connect(sid, environ):
 
 def send_control(steering_angle, throttle):
     sio.emit("steer", data={
-    'steering_angle': steering_angle.__str__(),
-    'throttle': throttle.__str__()
+        'steering_angle': steering_angle.__str__(),
+        'throttle': throttle.__str__()
     }, skip_sid=True)
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Remote Driving')
     parser.add_argument('model', type=str,
-    help='Path to model definition json. Model weights should be on the same path.')
+                        help='Path to model definition json. Model weights should be on the same path.')
     args = parser.parse_args()
     with open(args.model, 'r') as jfile:
         model = model_from_json(json.load(jfile))
